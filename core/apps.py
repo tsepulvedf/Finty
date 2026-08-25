d828b9677f3b799c3afcc52a0bf9ac391bbe1298
Tomas Sepulveda Franco <85259415+tsepulvedf@users.noreply.github.com>
2026-08-25 15:41:30 -0500

    Delete core directory

--- core/apps.py
+++
@@ -1,9 +0,0 @@
-"""Configuracion de la app `core`."""
-from django.apps import AppConfig
-
-
-class CoreConfig(AppConfig):
-    """App transversal: value objects, excepciones y utilidades compartidas."""
-
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "core"

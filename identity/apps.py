d4ea5bf45216403829404224230268a34824958a
Tomas Sepulveda Franco <85259415+tsepulvedf@users.noreply.github.com>
2026-08-25 15:42:33 -0500

    Delete identity directory

--- identity/apps.py
+++
@@ -1,9 +0,0 @@
-"""Configuracion de la app `identity`."""
-from django.apps import AppConfig
-
-
-class IdentityConfig(AppConfig):
-    """UserIdentityContext: identidad y perfil del usuario."""
-
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "identity"
